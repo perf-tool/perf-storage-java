5b9224daf8f8d3974d7b5f6c352930ababab0b79
Lico-Tom <583566138@qq.com>
2022-08-14 19:51:43 +0800

    add redis impl

---
+++ perf-storage-redis/src/main/java/com/github/perftool/storage/redis/service/RedisBootService.java
@@ -0,0 +1,106 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.github.perftool.storage.redis.service;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import com.github.perftool.storage.common.utils.IDUtils;
+import com.github.perftool.storage.redis.config.RedisConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.stereotype.Service;
+import java.time.Duration;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+@Slf4j
+@Service
+public class RedisBootService {
+
+    @Autowired
+    private RedisConfig redisConfig;
+
+    private RedisTemplate<String, Object> redisTemplate;
+
+    Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+
+    public void boot() {
+        this.redisTemplate = createRedisTemplate();
+        List<String> ids = IDUtils.getTargetIds(redisConfig.dataSetSize);
+        RedisOperations redisOperations = new RedisOperations(ids, redisConfig, redisTemplate);
+        ids.forEach(redisOperations::insertData);
+        ExecutorService executorService = Executors.newFixedThreadPool(redisConfig.fixedThreadNum);
+        for (int i = 0; i < redisConfig.fixedThreadNum; i++) {
+            executorService.execute(new RedisOperations(ids, redisConfig, redisTemplate));
+        }
+    }
+
+    private LettuceConnectionFactory lettuceConnectionFactory() {
+        GenericObjectPoolConfig genericObjectPoolConfig = new GenericObjectPoolConfig();
+        genericObjectPoolConfig.setMaxIdle(redisConfig.maxIdle);
+        genericObjectPoolConfig.setMinIdle(redisConfig.minIdle);
+        genericObjectPoolConfig.setMaxTotal(redisConfig.maxActive);
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setDatabase(redisConfig.database);
+        redisStandaloneConfiguration.setHostName(redisConfig.host);
+        redisStandaloneConfiguration.setPort(redisConfig.port);
+        redisStandaloneConfiguration.setPassword(RedisPassword.of(redisConfig.password));
+        LettuceClientConfiguration clientConfig = LettucePoolingClientConfiguration.builder()
+                .commandTimeout(Duration.ofMillis(redisConfig.timeout))
+                .shutdownTimeout(Duration.ofMillis(redisConfig.shutDownTimeout))
+                .poolConfig(genericObjectPoolConfig)
+                .build();
+
+        LettuceConnectionFactory factory = new LettuceConnectionFactory(redisStandaloneConfiguration, clientConfig);
+        factory.afterPropertiesSet();
+        return factory;
+    }
+
+    private RedisTemplate<String, Object> createRedisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(lettuceConnectionFactory());
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        mapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance,
+                ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+        jackson2JsonRedisSerializer.setObjectMapper(mapper);
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        template.setKeySerializer(stringRedisSerializer);
+        template.setHashKeySerializer(stringRedisSerializer);
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+        template.setHashValueSerializer(jackson2JsonRedisSerializer);
+        template.afterPropertiesSet();
+        return template;
+    }
+}
