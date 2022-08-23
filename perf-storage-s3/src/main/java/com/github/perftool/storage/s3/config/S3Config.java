509a6949e197d2f4b63673ed1dfef7fe4aca6e49
LeKe <95280282+goflutterjava@users.noreply.github.com>
2022-08-23 20:53:52 +0800

    Update S3Config.java

--- perf-storage-s3/src/main/java/com/github/perftool/storage/s3/config/S3Config.java
+++ perf-storage-s3/src/main/java/com/github/perftool/storage/s3/config/S3Config.java
@@ -33,6 +33,9 @@ public class S3Config extends CommonConfig {
 
     @Value("${AWS_SECRET_KEY:password}")
     public String secretKey;
+    
+    @Value("${SIGNER_OVERRIDE_TYPE:AWSS3V4SignerType}")
+    public String signerOverrideType;
 
     @Value("${BUCKET_NAME:testbucket}")
     public String bucketName;
@@ -48,9 +51,6 @@ public class S3Config extends CommonConfig {
 
     @Value("${DATA_SIZE:10240}")
     public int dataSize;
-
-    @Value("${SIGNER_OVERRIDE_TYPE:AWSS3V4SignerType}")
-    public String signerOverrideType;
 
 }
 
